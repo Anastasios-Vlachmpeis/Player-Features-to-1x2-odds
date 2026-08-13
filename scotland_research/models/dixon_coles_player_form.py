d147679f91ad058efb4b09ef9c4ea90f57f313ff
Tasos <vlahbeis.tasos@gmail.com>
2026-08-13 14:09:40 +0200

    ...

--- scotland_research/models/dixon_coles_player_form.py
+++ scotland_research/models/dixon_coles_player_form.py
@@ -12,11 +12,12 @@ from models.dixon_coles_core import DixonColesEstimator, PLAYER_L2_STRENGTH
 class DixonColesPlayerFormModel:
     name = "dixon_coles_player_form"
 
-    def __init__(self) -> None:
-        self._estimator = DixonColesEstimator(
-            player_features=PLAYER_FEATURES,
-            player_l2_strength=PLAYER_L2_STRENGTH,
-        )
+    def __init__(self, player_features=None, name="dixon_coles_player_form") -> None:
+
+        selected = (PLAYER_FEATURES if player_features is None else list(player_features))
+
+        self.name = name
+        self._estimator = DixonColesEstimator(player_features=selected, player_l2_strength=PLAYER_L2_STRENGTH)
 
     def fit(self, train: pd.DataFrame) -> None:
         self._estimator.fit(train)
